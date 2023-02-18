54e0b4ceeff5bf0eaf2916301ad231218d980d55
Sergey Filimonov <srfilimonoff@gmail.com>
2023-02-18 21:39:17 +0300

    replacing var on let

--- src/js/player.js
+++ src/js/player.js
@@ -1,70 +1,69 @@
-var video = document.getElementById('video'),
-  controls = document.getElementsByClassName('controls')[0],
-  playPause = document.getElementsByClassName('playpause')[0],
-  progress = document.getElementsByClassName('progress')[0],
-  volume = document.getElementsByClassName('volume-input')[0],
-  icon = document.getElementsByClassName('player__icon')[0],
-  volumeIcon = document.getElementsByClassName('volume__icon')[0],
+let video = document.getElementById("video"),
+  controls = document.getElementsByClassName("controls")[0],
+  playPause = document.getElementsByClassName("playpause")[0],
+  progress = document.getElementsByClassName("progress")[0],
+  volume = document.getElementsByClassName("volume-input")[0],
+  icon = document.getElementsByClassName("player__icon")[0],
+  volumeIcon = document.getElementsByClassName("volume__icon")[0],
   updateProgress;
 
-playPause.addEventListener('click', function () {
-  if (playPause.className == 'playpause pause') {
-    playPause.className = 'playpause play';
-    icon.style.display = 'none';
+playPause.addEventListener("click", function () {
+  if (playPause.className == "playpause pause") {
+    playPause.className = "playpause play";
+    icon.style.display = "none";
     video.play();
   } else {
-    playPause.className = 'playpause pause';
+    playPause.className = "playpause pause";
     video.pause();
-    icon.style.display = 'block';
+    icon.style.display = "block";
   }
 });
 
-icon.addEventListener('click', function () {
-  playPause.className = 'playpause play';
-  icon.style.display = 'none';
+icon.addEventListener("click", function () {
+  playPause.className = "playpause play";
+  icon.style.display = "none";
   video.play();
 });
 
-video.addEventListener('click', function () {
-  if (playPause.className == 'playpause pause') {
-    playPause.className = 'playpause play';
-    icon.style.display = 'none';
+video.addEventListener("click", function () {
+  if (playPause.className == "playpause pause") {
+    playPause.className = "playpause play";
+    icon.style.display = "none";
     video.play();
   } else {
-    playPause.className = 'playpause pause';
+    playPause.className = "playpause pause";
     video.pause();
-    icon.style.display = 'block';
+    icon.style.display = "block";
   }
 });
 
-video.addEventListener('play', function () {
+video.addEventListener("play", function () {
   progress.max = Math.round(video.duration * 10);
   updateProgress = setInterval(function () {
     progress.value = video.currentTime * 10;
   }, 100);
 });
 
-volume.addEventListener('input', function () {
-  video.volume = volume.value/100;
+volume.addEventListener("input", function () {
+  video.volume = volume.value / 100;
 });
 
-progress.addEventListener('input', function () {
+progress.addEventListener("input", function () {
   updateProgress = null;
   video.currentTime = progress.value / 10;
 });
 
+let currentVolume;
 
-var currentVolume;
-
-volumeIcon.addEventListener('click', function () {
+volumeIcon.addEventListener("click", function () {
   if (video.volume === 0) {
-    video.volume = currentVolume/100;
+    video.volume = currentVolume / 100;
     volume.value = currentVolume;
-    this.style.fill = 'white';
+    this.style.fill = "white";
   } else {
     currentVolume = volume.value;
     video.volume = 0;
     volume.value = 0;
-    this.style.fill = 'red';
+    this.style.fill = "red";
   }
-})
+});
